d4de8c97c0793150fe147b1234469cca6846bd01
zhonghp under Mint <zhonghp@foxmail.com>
2014-04-30 10:30:35 +0800

    finish first week's assignment

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,42 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = 
+    if (r==0 || c==0 || r==c) 1 
+    else pascal(c-1, r-1) + pascal(c, r-1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def balanceIter(leftCnt: Int, chars: List[Char]): Boolean = 
+      if (chars.isEmpty) leftCnt == 0 
+      else if (chars.head == '(') balanceIter(leftCnt+1, chars.tail)
+      else if (chars.head == ')') {
+        if (leftCnt > 0) balanceIter(leftCnt-1, chars.tail) else false
+      } else balanceIter(leftCnt, chars.tail)
+      
+    balanceIter(0, chars)
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = 
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money, coins.tail) + countChange(money-coins.head, coins)
+}
